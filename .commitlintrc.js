9541137df9c1863dfb5791cb0e8a7e5cd55e31c3
betaboon <betaboon@0x80.ninja>
2021-03-06 15:03:17 +0100

    repo(commitlint): add release-type and semantic-release-scope

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -13,6 +13,7 @@ module.exports = {
         'fix',
         'perf',
         'refactor',
+        'release',
         'repo',
         'revert',
       ],
@@ -21,7 +22,13 @@ module.exports = {
     'scope-enum': [
       2,
       'always',
-      ['.gitignore', 'commitlint', 'package.json', 'pnpm-workspace.yaml'],
+      [
+        '.gitignore',
+        'commitlint',
+        'package.json',
+        'pnpm-workspace.yaml',
+        'semantic-release',
+      ],
     ],
   },
   plugins: ['scope-empty-exceptions', 'workspace-scopes'],
