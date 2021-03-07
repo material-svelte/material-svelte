178d015eef3d6eafe0c189ccfcdaa98004c26e87
betaboon <betaboon@0x80.ninja>
2021-03-08 00:04:35 +0100

    repo(eslint): setup eslint-plugin-svelte3

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,15 @@ module.exports = {
     'standard',
     'prettier',
   ],
-  plugins: ['import'],
+  plugins: ['import', 'svelte3'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
 };
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*': 'prettier --plugin-search-dir=. --ignore-unknown --write',
-  '*.js': 'eslint --ext .js --fix',
+  '*.{js,svelte}': 'eslint --ext .js,.svelte --fix',
 };
