42504d21180813201113716a08c9f0b03e5e6e3b
betaboon <betaboon@0x80.ninja>
2021-03-09 17:35:38 +0100

    repo: comply to vercel-sponsorship requirements

---
+++ apps/website/src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<footer>
+  <a
+    href="https://vercel.com?utm_source=material-svelte&utm_campaign=oss"
+    target="_blank"
+  >
+    <img src="powered-by-vercel.svg" alt="Powered by Vercel" />
+  </a>
+</footer>
+
+<style>
+  footer {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    text-align: center;
+  }
+</style>
