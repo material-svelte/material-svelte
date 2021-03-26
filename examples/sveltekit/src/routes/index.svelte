e2f531b4735a03424b995712f9abf344566af7a2
betaboon <betaboon@0x80.ninja>
2021-03-26 17:16:47 +0100

    feat(material-svelte-example-sveltekit): initial implementation

---
+++ examples/sveltekit/src/routes/index.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import Counter from '$lib/Counter.svelte';
+  import Button from '@material-svelte/button';
+  import Icon from '@material-svelte/icon';
+  import { mdiHome } from '@mdi/js';
+</script>
+
+<main>
+  <h1>Hello world!</h1>
+
+  <Counter />
+  <Button>
+    <Icon slot="icon" path={mdiHome} />
+    Button
+  </Button>
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    margin: 0 auto;
+    padding: 1em;
+    text-align: center;
+  }
+
+  h1 {
+    color: #ff3e00;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 4rem auto;
+    max-width: 14rem;
+    text-transform: uppercase;
+  }
+
+  p {
+    line-height: 1.35;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
