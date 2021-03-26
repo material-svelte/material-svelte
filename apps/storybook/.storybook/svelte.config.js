65332e9ab648e42cbce68ff6af9f27da7d2cc684
betaboon <betaboon@0x80.ninja>
2021-03-26 21:06:03 +0100

    fix(material-svelte-storybook): build with svelte-preprocess 4.7.0

--- apps/storybook/.storybook/svelte.config.js
+++ apps/storybook/.storybook/svelte.config.js
@@ -2,5 +2,8 @@ const sveltePreprocess = require('svelte-preprocess');
 module.exports = {
   preprocess: sveltePreprocess({
     postcss: true,
+    typescript: {
+      tsconfigFile: false,
+    },
   }),
 };
