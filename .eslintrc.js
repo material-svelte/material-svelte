216da8a99ee052f2fa704b311a1c0a35ccf8a10b
betaboon <betaboon@0x80.ninja>
2021-03-26 16:48:18 +0100

    repo(eslint): separate configs for *.js, *.svelte, *.stories.svelte

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,25 +5,52 @@ module.exports = {
     jest: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'standard',
-    'prettier',
-  ],
-  plugins: ['import', 'svelte3'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   overrides: [
     {
+      files: ['*.js'],
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'standard',
+        'prettier',
+      ],
+      plugins: ['import'],
+    },
+    {
       files: ['*.svelte'],
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'standard-with-typescript',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+      ],
+      plugins: ['import', 'svelte3', '@typescript-eslint'],
+      parser: '@typescript-eslint/parser',
       processor: 'svelte3/svelte3',
       rules: {
-        // TODO figure out how to get this working without ignoring.
-        // currently required for sveltekit
-        'import/no-unresolved': ['error', { ignore: ['^\\$'] }],
+        // TODO figure out why this is not working
+        'import/no-unresolved': 'off',
+        // import order cannot be determined correctly in .svelte components
+        'import/first': 'off',
+      },
+      settings: {
+        'svelte3/typescript': require('typescript'),
+        'svelte3/ignore-styles': (attributes) =>
+          attributes.lang && attributes.foo !== 'css',
+      },
+    },
+    {
+      files: ['*.stories.svelte'],
+      rules: {
+        // ignore dependencies of stories within components
+        // as they are not listed in the package but in storybook
+        'import/no-unresolved': 'off',
       },
     },
   ],
