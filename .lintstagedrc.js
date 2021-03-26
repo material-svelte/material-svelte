540c3db0e049dc324e4fa6bd8eed5a6cdddca251
betaboon <betaboon@0x80.ninja>
2021-03-26 16:50:19 +0100

    repo(stylelint): lint .svelte

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*': 'prettier --plugin-search-dir=. --ignore-unknown --write',
   '*.{js,svelte}': 'eslint --ext .js,.svelte --fix',
-  '*.{css,scss}': 'stylelint --fix',
+  '*.{css,scss,svelte}': 'stylelint --fix',
 };
