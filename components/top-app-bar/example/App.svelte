7559c33e836d8f22bd4b602f579b12c4c09f88f4
betaboon <betaboon@0x80.ninja>
2021-04-16 18:36:28 +0200

    refactor(@material-svelte/top-app-bar): adapt IconButton

--- components/top-app-bar/example/App.svelte
+++ components/top-app-bar/example/App.svelte
@@ -1,34 +1,19 @@
 <script lang="ts">
   import TopAppBar from '../src/TopAppBar.svelte';
-  import Button from '@material-svelte/button';
-  import Icon from '@material-svelte/icon';
+  import { IconButton } from '@material-svelte/button';
   import { Typography } from '@material-svelte/typography';
   import { mdiMenu, mdiShareVariant, mdiMagnify } from '@mdi/js';
-
-  let prominent = false;
 </script>
 
 <svelte:head>
   <link href="https://unpkg.com/ress/dist/ress.min.css" rel="stylesheet" />
 </svelte:head>
 
-<TopAppBar {prominent}>
-  <Button
-    slot="icon"
-    variant="menu"
-    on:click={() => {
-      prominent = !prominent;
-    }}
-  >
-    <Icon slot="icon" path={mdiMenu} />
-  </Button>
+<TopAppBar color="#ccc">
+  <IconButton slot="icon" path={mdiMenu} />
   <Typography slot="title" variant="h6">Page Title</Typography>
-  <span slot="actions">
-    <Button slot="icon" variant="menu">
-      <Icon slot="icon" path={mdiShareVariant} />
-    </Button>
-    <Button slot="icon" variant="menu">
-      <Icon slot="icon" path={mdiMagnify} />
-    </Button>
-  </span>
+  <slot slot="actions">
+    <IconButton path={mdiShareVariant} />
+    <IconButton path={mdiMagnify} />
+  </slot>
 </TopAppBar>
