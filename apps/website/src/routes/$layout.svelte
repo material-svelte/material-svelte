a390df90e6aab1021309f68b73ec38807ef2fa13
betaboon <betaboon@0x80.ninja>
2021-04-21 15:55:42 +0200

    feat(material-svelte-website): apply branding

--- apps/website/src/routes/$layout.svelte
+++ apps/website/src/routes/$layout.svelte
@@ -5,12 +5,17 @@
 <main>
   <slot />
 </main>
+
 <Footer />
 
 <style>
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  :global(html, body) {
+    background-color: #484854;
   }
 
   main {
