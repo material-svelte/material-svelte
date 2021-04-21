a6526f73e80a7b4112c765549b99a5052c09df50
betaboon <betaboon@0x80.ninja>
2021-04-21 13:03:20 +0200

    feat(@material-svelte/postcss-plugin): support options

--- tools/postcss-plugin/src/index.js
+++ tools/postcss-plugin/src/index.js
@@ -1,38 +1,29 @@
 const postcss = require('postcss');
+const { cosmiconfigSync } = require('cosmiconfig');
 
-const materialSvelteOptionsPlugin = (options) => {
-  const packageRE = /.*@material-svelte\/(?<package>\w+)\/.*/;
-  return {
-    postcssPlugin: 'material-svelte-options',
-    Declaration(node) {
-      if (!node.prop.startsWith('--')) {
-        return;
-      }
-      const match = node.source.input.file.match(packageRE);
-      if (!match) {
-        return;
-      }
-      try {
-        const component = match.groups.package;
-        const propName = node.prop.substring(2);
-        const propValue = options.components[component][propName];
-        if (propValue) {
-          node.value = propValue;
-        }
-      } catch {
-        /* */
-      }
-    },
-  };
-};
-materialSvelteOptionsPlugin.postcss = true;
+const defaultConfig = {};
 
-module.exports = (options) => {
+function getConfig() {
+  const explorer = cosmiconfigSync('material');
+  try {
+    const result = explorer.search();
+    if (result) {
+      return result.config;
+    } else {
+      return defaultConfig;
+    }
+  } catch (error) {
+    console.error('Failed to load material-configuration:', error);
+  }
+}
+
+module.exports = () => {
+  const options = getConfig();
   const plugins = [
-    materialSvelteOptionsPlugin(options),
+    require('./custom-properties')(options),
     require('postcss-import'),
     require('postcss-mixins')({
-      mixins: require('@material-svelte/postcss-mixins'),
+      mixins: require('@material-svelte/postcss-mixins')(options),
     }),
     require('postcss-nested'),
     require('postcss-simple-vars'),
