a7d99b6d2a79f7d6df49e80896a2dad2b23bb513
betaboon <betaboon@0x80.ninja>
2021-05-01 20:53:01 +0200

    feat(material-svelte-example): add example usage for Radio-component

--- apps/example/src/routes/rail/index.svelte
+++ apps/example/src/routes/rail/index.svelte
@@ -7,6 +7,7 @@
     ListSubheader,
   } from '@material-svelte/list';
   import { Switch } from '@material-svelte/switch';
+  import { Radio } from '@material-svelte/radio';
 
   const { divider, compact, align } = getContext('$rail');
 </script>
@@ -28,5 +29,30 @@
   >
     compact
     <Switch slot="meta" bind:checked={$compact} />
+  </ListItem>
+  <ListSubheader>NavigationRail alignment</ListSubheader>
+  <ListItem
+    on:click={() => {
+      $align = 'start';
+    }}
+  >
+    start
+    <Radio slot="meta" bind:group={$align} value="start" />
+  </ListItem>
+  <ListItem
+    on:click={() => {
+      $align = 'center';
+    }}
+  >
+    center
+    <Radio slot="meta" bind:group={$align} value="center" />
+  </ListItem>
+  <ListItem
+    on:click={() => {
+      $align = 'end';
+    }}
+  >
+    end
+    <Radio slot="meta" bind:group={$align} value="end" />
   </ListItem>
 </List>
